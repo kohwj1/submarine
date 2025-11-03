fc15bb69b69fd740fb30db177e2e325238716c63
Arkeil <kohwj1@gmail.com>
2025-11-03 11:00:17 +0900

    fixed #10

--- static/js/weatherDetail.js
+++ static/js/weatherDetail.js
@@ -12,19 +12,13 @@ function getProgressBar() {
             clearInterval(timer);
             location.reload();
         } else {
-            currentListItem.style.background = `linear-gradient(to right, #d9e8ff ${barPercent}%, transparent ${100 - barPercent}%)`;
-            // currentListItem.setAttribute('data-bs-title', `${Math.floor(timeDiff / 60)}분 ${Math.floor(timeDiff % 60)}초 후에 날씨가 변경됩니다.`)
+            currentListItem.style.background = `linear-gradient(to right, #d9e8ff ${barPercent}%, transparent ${barPercent}%)`;
 
             currentListItem.setAttribute('aria-valuenow', barPercent);
         }
-    }, 1000);
+    }, 100);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    // currentListItem.setAttribute('data-bs-toggle', 'tooltip');
-    // currentListItem.setAttribute('data-bs-placement', 'bottom');
-    // currentListItem.setAttribute('data-bs-title', '-');
-    // const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-    // const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
     getProgressBar();
 });
