090215e1c8a1d14750045bbf5d640aeb6c1b65ae
Arkeil <kohwj1@gmail.com>
2025-11-04 14:29:49 +0900

    added index page UI

---
+++ static/js/index.js
@@ -0,0 +1,22 @@
+const etText = document.getElementById('timer');
+const etTextNormal = document.getElementById('normal-clock');
+const timeType = document.querySelector('.eorzea.timetype')
+
+function getEorzeaTime() {
+    let timer = setInterval(() => {
+        const EORZEA_MULTIPLIER = 3600 / 175;
+        const epoch = new Date(1970, 1, 1, 0, 0, 0, 0).getTime();
+        let now = new Date().getTime();
+
+        let elapsedEorzeaTime = (now - epoch) * EORZEA_MULTIPLIER;
+
+        let eorzeaTime = new Date(epoch + elapsedEorzeaTime);
+        let et_hour = String(eorzeaTime.getHours()).padStart(2, '0');
+        let et_minute = String(eorzeaTime.getMinutes()).padStart(2, '0');
+        timeType.textContent = "ET";
+        etText.textContent = `${et_hour}:${et_minute}`;
+        etTextNormal.textContent = `ET ${et_hour}:${et_minute}`;
+    }, 1000);
+}
+
+document.addEventListener("DOMContentLoaded", getEorzeaTime)
