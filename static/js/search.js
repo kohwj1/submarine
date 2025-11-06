871bd3bf8d15fc9801aa3e592e567577a80d1086
Arkeil <kohwj1@gmail.com>
2025-11-06 11:03:17 +0900

    added submarine reward text filter

--- static/js/search.js
+++ static/js/search.js
@@ -1,6 +1,23 @@
+const keywordInput = document.getElementById('filter')
 const itemList = document.querySelectorAll('.item-name')
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('hi?')
+keywordInput.addEventListener('input', function() {
+    const searchKeyword = keywordInput.value;
+    // console.log(searchKeyword);
+    if (searchKeyword == "") {
+        for (item of itemList) {
+            let tableRow = item.parentElement.parentElement;
+            tableRow.style.display = "table-row";
+        }
+    } else {
+        for (item of itemList) {
+            let tableRow = item.parentElement.parentElement;
+            if (item.textContent.includes(searchKeyword)) {
+                tableRow.style.display = "table-row";
+            } else {
+                tableRow.style.display = "none";
+            }
+        }
+    }
 })
