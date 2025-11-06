9fb7565281d9dbc9dd9c7513c1b818c23abe0485
Arkeil <kohwj1@gmail.com>
2025-11-06 11:47:14 +0900

    added remain time text in weather detail page

--- static/js/weatherDetail.js
+++ static/js/weatherDetail.js
@@ -1,4 +1,5 @@
 const currentListItem = document.querySelectorAll('.remain-time')[0]
+const nextWeatherTimeUi = document.querySelector('.next-weather-time')
 
 function getProgressBar() {
     const strNextWeatherTime = document.querySelectorAll('.unix-time')[1].textContent.replace(' ','T');
@@ -13,8 +14,8 @@ function getProgressBar() {
             location.reload();
         } else {
             currentListItem.style.background = `linear-gradient(to right, #d9e8ff ${barPercent}%, transparent ${barPercent}%)`;
-
             currentListItem.setAttribute('aria-valuenow', barPercent);
+            nextWeatherTimeUi.textContent = `${Math.floor(timeDiff / 60)}분 ${Math.floor(timeDiff % 60)}초 후 날씨 변경`
         }
     }, 100);
 }
